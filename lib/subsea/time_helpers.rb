bf1c6c6e403f4bb609e60c3a263a8ab0adf8821d
Chris Jones <chris@richmondday.com>
2010-08-17 06:02:41 -0400

    fixing some formatting

--- lib/subsea/time_helpers.rb
+++ lib/subsea/time_helpers.rb
@@ -1,22 +1,22 @@
 module TimeHelpers
-		def time
-			start = Time.now
-			yield
-			@finish = Time.now - start
-		end
-		
-		def get_ticks_for_display_from_time(time_string)
-			ticks = "seconds"
-			ticks = "minutes" if time_string > 60
-			
-			return ticks
-		end
-		
-		def format_time_for_display(time_string)
-			time_string = time_string / 60 if time_string > 60
-		
-			time_string = (time_string * 100).round / 100.0		
-			
-			return time_string
-		end  
+  def time
+    start = Time.now
+    yield
+    @finish = Time.now - start
+  end
+
+  def get_ticks_for_display_from_time(time_string)
+    ticks = "seconds"
+    ticks = "minutes" if time_string > 60
+
+    return ticks
+  end
+
+  def format_time_for_display(time_string)
+    time_string = time_string / 60 if time_string > 60
+
+    time_string = (time_string * 100).round / 100.0		
+
+    return time_string
+  end  
 end
